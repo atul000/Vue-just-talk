6ebee56c9841a55da11f2f2db2ba2c1483934ae7
atul beniwal <33995896+atul000@users.noreply.github.com>
2020-01-27 22:58:01 +0530

    Just Chat

---
+++ src/firebase/init.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBVDQTfKFoTUPX31f8ZVW4TnBCFpEq8nG4",
+  authDomain: "face-detection-c3e0c.firebaseapp.com",
+  databaseURL: "https://face-detection-c3e0c.firebaseio.com",
+  projectId: "face-detection-c3e0c",
+  storageBucket: "face-detection-c3e0c.appspot.com",
+  messagingSenderId: "916300921188",
+  appId: "1:916300921188:web:001015c34e39520e9e6271"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
